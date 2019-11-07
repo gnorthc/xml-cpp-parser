837c72a3d9d6bd7c152c54b951f5aed2c333077f
gnorthc <47445888+gnorthc@users.noreply.github.com>
2019-11-07 09:32:14 +0800

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include "pch.h"
+#include <iostream>
+#include <time.h>
+#include "xml2.h"
+
+
+int main()
+{
+	
+	setlocale(LC_ALL, "");
+
+	// 支持三种编码格式：aqx::xts_utf16 aqx::xts_utf8 aqx::xts_asc
+	aqx::xdoc<aqx::xts_utf16> doc;
+	auto t = clock();
+	int err = doc.load_file(L"G:\\vs2017\\POE64_V1\\生成\\pfile.xml");
+	printf("解析文档耗时：%I64d ms\n", clock() - t);
+	if (err) {
+		printf("%s\n", doc.get_error_info().c_str());
+		return 0;
+	}
+	auto e = doc.get_element(L"CATALOG2");
+	printf("%ls\n", e.get_inner_xml().c_str());
+
+	return 0;
+}
