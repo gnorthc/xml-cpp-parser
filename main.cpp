0db7b3b90418b37b8549d2f6123595b40966764f
gnorthc <47445888+gnorthc@users.noreply.github.com>
2019-11-07 14:31:39 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -10,6 +10,7 @@ int main()
 	setlocale(LC_ALL, "");
 
 	// 支持三种编码格式：aqx::xts_utf16 aqx::xts_utf8 aqx::xts_asc
+	// aqx这个命名空间只是我个人的库，没有什么特别含义，只是顺手，能用左手2个手指快速打出来而已。
 	aqx::xdoc<aqx::xts_utf16> doc;
 	auto t = clock();
 	int err = doc.load_file(L"G:\\vs2017\\POE64_V1\\生成\\pfile.xml");
